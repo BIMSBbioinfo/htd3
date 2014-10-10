f3bebec2e055855d24c87c4d0fbc1aae78b62f23
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2014-10-10 16:51:29 +0200

    remove unused settings parameter

--- htd3.js
+++ htd3.js
@@ -311,7 +311,6 @@ chr11	31804689	31807426	NR_117094	0	+	31807426	31807426	0	1	2737,	0,
         settings = {
           extent: undefined,
           trackHeight: 15,
-          linkRadiusRatio: 0.8,
           width: 800,
           paddingX: 50,
           paddingY: 50,
