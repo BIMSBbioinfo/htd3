af0fff5692e14ecc7db0915a37a36f7d667f6058
Ricardo Wurmus <ricardo.wurmus@mdc-berlin.de>
2014-10-15 10:24:37 +0200

    change outdated comment

--- htd3.js
+++ htd3.js
@@ -473,7 +473,7 @@ chr11	31804689	31807426	NR_117094	0	+	31807426	31807426	0	1	2737,	0,
           .ticks(10)
       };
 
-      // draw associations and adjust track height
+      // draw associations
       function drawExonIntron (d, i) {
         var context = d3.select(this);
 
